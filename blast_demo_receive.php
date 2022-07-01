9922ceadac3fcf2bbe51ed96387d51df984b7d30
Wallen01 <wangallen0517@gmail.com>
2022-07-01 11:34:11 +0800

    fopen not done

--- blast_demo_receive.php
+++ blast_demo_receive.php
@@ -8,11 +8,18 @@
 
 <body>
     <?php
-    if (isset($_POST["query_seq"])) {
-        $query_seq = $_POST["query_seq"];
+    if (!isset($_POST["query_seq"])) {
+        echo "No query.";
+        exit();
+    }
+    $query_seq = $_POST["query_seq"];
+    if (!stripos($query_seq, '>', stripos($query_seq, '>'))===false) {
+        echo "Only One Query.";
+        exit();
     }
     if (isset($_POST["evalue"])) {
         $evalue = $_POST["evalue"];
+        
     }
     if (isset($_POST["blast_type"])) {
         $blast_type = $_POST["blast_type"];
@@ -20,15 +27,24 @@
     if (isset($_POST["species"])) {
         $species = $_POST["species"];
     }
-    $link = new mysqli('140.116.56.177', 'onlineDB', 'bidlab711', 'plantpan4') or die("Unable to connect the database."); //連接資料庫
+    echo "
+    query_seq=$query_seq;<br>
+    evalue=$evalue;<br>
+    blast_type=$blast_type<br>
+    species=$species<br>
+    ";
+    print_r($species);
+    // $link = new mysqli('140.116.56.177', 'onlineDB', 'bidlab711', 'plantpan4') or die("Unable to connect the database."); //連接資料庫
     //decide blast n or p
-    if($_POST["species"]=="n"){
-        $query="blastn -db $species -query $query_seq -out text -outfmt 6";
-    }else if($_POST["species"]=="p"){
-        $query="blastp -db $species -query $query_seq -out text -outfmt 6";
-    } 
-    $query="blastn";
-    system('ls');
+    if ($_POST["species"] == "n") {
+        $query = "blastn -db $species -query $query_seq -out text -outfmt 6";
+    } else if ($_POST["species"] == "p") {
+        $query = "blastp -db $species -query $query_seq -out text -outfmt 6";
+    }
+    echo $query;
+    // if(!system($query)){
+    //     echo "blast fail.";
+    // }
     ?>
 </body>
 
--- blast_demo_send.php
+++ blast_demo_send.php
@@ -14,9 +14,12 @@
     <form action="blast_demo_receive.php" method="post">
         Input a sequence: <br>
         <textarea type="text" name="query_seq"></textarea><br>
-        E-value: <input type="text" name="evalue"><br>
-        Blast type<input type="radio" name="blast_type"><br>
+        E-value: <input type="text" name="evalue" onkeypress="return (event.charCode !=8 && event.charCode ==0 || ( event.charCode == 46 || (event.charCode >= 48 && event.charCode <= 57)))" ><br>
+        Blast type<br>
+        <label><input type="radio" name="blast_type" value="n">N to N</label><br>
+        <label><input type="radio" name="blast_type" value="p">P to P</label><br>
         which database: <input id="dbselect" name="species"><br>
+        <input type="submit">
     </form>
 </body>
 <!--nesscery for scrabble box-->
