f8dc6723e30209a90effd5492e5a2beccaa0a3cb
Wallen01 <wangallen0517@gmail.com>
2022-07-01 17:05:42 +0800

    blast fail

--- blast_demo_receive.php
+++ blast_demo_receive.php
@@ -13,13 +13,12 @@
         exit();
     }
     $query_seq = $_POST["query_seq"];
-    if (!stripos($query_seq, '>', stripos($query_seq, '>')+1)===false) {
+    if (!stripos($query_seq, '>', stripos($query_seq, '>') + 1) === false) {
         echo "Only One Query.";
         exit();
     }
     if (isset($_POST["evalue"])) {
         $evalue = $_POST["evalue"];
-        
     }
     if (isset($_POST["blast_type"])) {
         $blast_type = $_POST["blast_type"];
@@ -33,36 +32,50 @@
     blast_type=$blast_type<br>
     species=$species<br>
     ";
-    
+    $id = time();
+    if (!$fp1 = fopen("./blastoutput/$id", "w")) {
+        echo "fail to open blastoutput/" . $id . "<br>";
+    } else {
+        fwrite($fp1, $query_seq);
+        fclose($fp1);
+    }
+
     print_r($species);
     //turn species to A_a format
-    for($i=0,$num=count($species);$i<$num;$i++){
-        $species[$i]=str_replace(" ","_",$species[$i]);
+    for ($i = 0, $num = count($species); $i < $num; $i++) {
+        $species[$i] = str_replace(" ", "_", $species[$i]);
     }
     echo "<br>";
     print_r($species);
-    
-    //decide blast n or p
-    $query="";
-    if ($blast_type == "n") {
-        $query = "blastn -db ".implode(" ",$species)." -query $query_seq -out text -outfmt 6";
-    } else if ($blast_type == "p") {
-        $query = "blastp -db ".implode(" ",$species)." -query $query_seq -out text -outfmt 6";
-    }
-    echo "<br>".$query."<br>";
-    $id=time();
-    if(!$fp=fopen("../blastinput/$id","w")){
-        echo "fail to open blastinput/".$id."<br>";
-    }
-    if(!$fp2=fopen("../blastoutput/$id","w")){
-        echo "fail to open blastoutput/".$id."<br>";
-    };
 
-    // if(!system($query)){
-        //     echo "blast fail.";
-        // }
-        // $link = new mysqli('140.116.56.177', 'onlineDB', 'bidlab711', 'plantpan4') or die("Unable to connect the database."); //連接資料庫
-        ?>
+    //decide blast n or p
+    echo "<br>" . $query . "<br>";
+    $query = "";
+    if ($blast_type == "n") {
+        $query = "blastn -db " . implode(" ", $species) . " -query " . $query_seq . " -out ./blastoutput/" . $id . ".txt -outfmt 6 -evalue $evalue";
+    } else if ($blast_type == "p") {
+        $query = "blastp -db " . implode(" ", $species) . " -query " . $query_seq . " -out ./blastoutput/" . $id . ".txt -outfmt 6 -evalue $evalue";
+    }
+
+
+
+    if (!system($query)) {
+        echo "blast fail.";
+    } else {
+
+        echo "<textarea>";
+        $blast_result = explode(PHP_EOL, file_get_contents("./blastoutput/$id"));
+        foreach($blast_result as $k => $v){
+            $blast_result[$k]=explode(chr(9),$v);
+        }
+        print_r($blast);
+        echo "</textarea>";
+    }
+
+
+
+    // $link = new mysqli('140.116.56.177', 'onlineDB', 'bidlab711', 'plantpan4') or die("Unable to connect the database."); //連接資料庫
+    ?>
 </body>
 
 </html>
